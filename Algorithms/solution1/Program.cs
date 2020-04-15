d03961d6e7f07308c3c31c891f16fa196927136a
shashanks4 <49721667+shashanks4@users.noreply.github.com>
2020-04-15 16:07:05 +0530

    Add files via upload

---
+++ Algorithms/solution1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System; 
+
+class NoOfPairs { 
+	
+	static void printPairs(int[] arr, 
+						int n, int sum) 
+	{ 
+		// int count = 0; 
+
+
+		for (int i = 0; i < n; i++) 
+			for (int j = i + 1; j < n; j++) 
+				if (arr[i] + arr[j] == sum) 
+					Console.Write("(" + arr[i] + ", " + arr[j] + ")"
+								+ "\n"); 
+								else {
+									 return ;
+								}
+				
+	} 
+
+
+	public static void Main() 
+	{ 
+        int i,n;
+		int[] arr = new int[100];
+        Console.Write("Input the number of elements to store in the array :");
+        n = Convert.ToInt32(Console.ReadLine());
+          Console.Write("Input {0} number of elements in the array :\n",n);
+          
+   for( i=0;i<n;i++)
+      {
+	  Console.Write("element - {0} : ",i);
+	  arr[i] = Convert.ToInt32(Console.ReadLine()); 
+	  } 
+		 n = arr.Length; 
+		int sum = 10; 
+		printPairs(arr, n, sum); 
+	} 
+} 
---
+++ Algorithms/solution2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System; 
+using System.Collections.Generic; 
+
+class FindPair{ 
+	static void printpairs(int[] arr, 
+						int sum) 
+	{ 
+		HashSet<int> s = new HashSet<int>(); 
+		for (int i = 0; i < arr.Length; ++i) { 
+			int temp = sum - arr[i]; 
+
+			
+			if (s.Contains(temp)) { 
+				Console.Write("Pair with given sum " + sum + " is (" + arr[i] + ", " + temp + ")\n"); 
+			} 
+			
+			s.Add(arr[i]); 
+		} 
+	} 
+
+	
+	static void Main() 
+	{ 
+		int[] A = new int[] { 8,7,2,5,3,1 }; 
+		int n = 10; 
+		printpairs(A, n); 
+	} 
+} 
+
