157a0b40902535b3618ec95b152e938ef77f9e63
shashanks4 <49721667+shashanks4@users.noreply.github.com>
2020-04-18 23:25:10 +0530

    Add files via upload

---
+++ Algorithms/solution3/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace solution3
+{
+    
+
+
+public class SumOfPairs 
+{ 
+
+	public void pairedElements(int []arr, int sum) 
+	{ 
+		int low = 0; 
+		int high = arr.Length - 1; 
+
+		while (low < high) 
+		{ 
+			if (arr[low] + arr[high] == sum) 
+			{ 
+				Console.WriteLine("The pair is : ("
+								+ arr[low] + ", " + arr[high] + ")"); 
+			} 
+			if (arr[low] + arr[high] > sum) 
+			{ 
+				high--; 
+			} 
+			else
+			{ 
+				low++; 
+			} 
+		} 
+	} 
+
+	// Driver code 
+	public static void Main(String[] args) 
+	{ 
+		int []arr = {  8,7,2,5,3,1 }; 
+		Array.Sort(arr); 
+
+		SumOfPairs sp = new SumOfPairs(); 
+		sp.pairedElements(arr, 10); 
+	} 
+} 
+
+
+}
